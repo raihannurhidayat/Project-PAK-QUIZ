25bbb9707a907ce59ea2986154c982d24d1768df
severusDude <fajarsaladin05@gmail.com>
2024-04-25 21:09:22 +0700

    add create question handler

--- server/django-quiz-app/qazkis/core/views.py
+++ server/django-quiz-app/qazkis/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Question
 
+from .forms import NewTestForm
+
 
 # Create your views here.
 
@@ -13,3 +15,19 @@ def get_question(request):
     question_json = list(question.values())
 
     return JsonResponse(question_json, safe=False)
+
+
+def create_question(request):
+
+    if request.method == "POST":
+        form = NewTestForm()
+
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'message': 'Data saved successfully'})
+        else:
+            errors = form.errors.as_json()
+            return JsonResponse({'error': errors}, status=400)
+
+    else:
+        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
